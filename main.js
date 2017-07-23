d1447fa4f8798418641d35329c85f71e3496e2af
yuta <yuta20030801@icloud.com>
2017-07-23 22:18:57 +0900

    remove 初期設定を廃止

--- main.js
+++ main.js
@@ -31,31 +31,25 @@ mongo.connect('mongodb://localhost:27017/issues', (error, db) => {
 https://github.com/yuta0801/issues-kun/wiki/Command`);
 		});
 
-		// 初期設定
-		addCommand(message, /^>init$/, msg => {
-			db.createCollection(message.channel.guild.id, (err, collection) => {
-				message.channel.send((error)?'エラー：'+error:'初期設定が完了しました。');
-			});
-		});
-
 		// 一覧表示
 		addCommand(message, /^>log( ([0-3]))?( (open|closed))?( ([^#]{2,32}#\d{4})))$?/, msg => {
-			let list = [], collection = db.collection(message.channel.guild.id);
-			collection.find().toArray((err, docs) => {
-				for (let doc of docs) {
-					if (msg[2] && doc.level != msg[2]) return;
-					if ((msg[4] && doc.status != msg[4]) || (!msg[4] && doc.status != 'open')) return;
-					if (msg[6] && doc.level != msg[6]) return;
-					list.push(`\`${doc.id}\`  ${level[doc.level]}  ${doc.title}  by ${doc.user}`);
-				}
-				message.channel.send((list.length>0)?list.join('\n'):'見つかりませんでした！');
+			let list = [];
+			db.createCollection(message.channel.guild.id, (err, collection) => {
+				collection.find().toArray((err, docs) => {
+					for (let doc of docs) {
+						if (msg[2] && doc.level != msg[2]) return;
+						if ((msg[4] && doc.status != msg[4]) || (!msg[4] && doc.status != 'open')) return;
+						if (msg[6] && doc.level != msg[6]) return;
+						list.push(`\`${doc.id}\`  ${level[doc.level]}  ${doc.title}  by ${doc.user}`);
+					}
+					message.channel.send((list.length>0)?list.join('\n'):'見つかりませんでした！');
+				});
 			});
 		});
 
 		// 投稿する
 		addCommand(message, /^>submit (.{2,20}) ([0-3])[ \n]([\s\S]+)$/, msg => {
-			let collection = db.collection(message.channel.guild.id);
-			collection.find().toArray((err, docs) => {
+			db.createCollection(message.channel.guild.id, (err, collection) => {
 				let ids = [];
 				for (let doc of docs) ids.push(doc.id);
 				collection.insertOne({
@@ -73,22 +67,42 @@ https://github.com/yuta0801/issues-kun/wiki/Command`);
 		});
 
 		addCommand(message, /^>show ([a-zA-Z0-9]{8})$/, msg => {
-			let collection = db.collection(message.channel.guild.id);
-			collection.findOne({id: msg[1]}, (err, doc) => {
-				console.log(doc);
-				message.channel.send(
+			db.createCollection(message.channel.guild.id, (err, collection) => {
+				collection.findOne({id: msg[1]}, (err, doc) => {
+					message.channel.send(
 `\`${doc.id}\`  ${level[doc.level]}  ${doc.title}
 
 ${doc.content}
 
 by ${doc.user}  ${doc.status}  ${doc.date.toFormat('YYYY/MM/DD HH24:MI:SS')}`);
+				});
 			});
 		});
 
 		addCommand(message, /^>close ([a-zA-Z0-9]{8})$/, msg => {
-			let collection = db.collection(message.channel.guild.id);
-			collection.updateOne({id: msg[1]}, {$set: {status: 'closed'}}, (err, result) => {
-				message.channel.send((error)?'エラー：'+error:`\`${msg[1]}\`を完了状態にしました。`);
+			db.createCollection(message.channel.guild.id, (err, collection) => {
+				collection.updateOne({id: msg[1]}, {$set: {status: 'closed'}}, (err, result) => {
+					message.channel.send((error)?'エラー：'+error:`\`${msg[1]}\`を完了状態にしました。`);
+				});
+			});
+		});
+
+		// 報告する
+		addCommand(message, /^>issue (.{2,20}) ([0-3])[ \n]([\s\S]+)$/, msg => {
+			db.createCollection(message.channel.guild.id, (err, collection) => {
+				let ids = [];
+				for (let doc of docs) ids.push(doc.id);
+				collection.insertOne({
+					id:      makeId(ids),
+					user:    message.author.tag,
+					title:   msg[1],
+					level:   msg[2],
+					content: msg[3],
+					status:  'open',
+					date:    new Date()
+				}, (error, result) => {
+					message.channel.send((error)?'エラー：'+error:'投稿しました。');
+				});
 			});
 		});
 	});
