9a38bfe14f4ee9623e568105bd9ec51397b6e271
yuta <yuta20030801@icloud.com>
2017-07-23 20:44:51 +0900

    add ユーザー指定 細かな修正

--- main.js
+++ main.js
@@ -14,49 +14,41 @@ client.on('ready', () => {
 mongo.connect('mongodb://localhost:27017/issues', (error, db) => {
 	client.on('message', message => {
 		// 説明表示
-		addCommand(message, />help( (\w+))?$^)?/, msg => {
+		addCommand(message, /^>help( (\w+))?$/, msg => {
 			message.channel.send(
-`\`>投稿 タイトル 重要度 内容\`
-値と値の間の区切り文字は、
-スペースやタブなどの空白文字でも構いません。
+`問題くんはそれぞれのDiscordのサーバー内でIssuesを管理できるBotです。
 \`\`\`
-タイトル
-	スペースやタブなどの空白文字は含まないでください。
-	また2文字以上、20文字以内でお書きください。
-重要度
-	${(() => {
-		let list = '';
-		for (let i=0; i<level.length; i++) {
-			list += `${level[i]} -> ${i}\n\t`;
-		}
-		return list;
-	})()}
-内容
-	改行や空白を入れても構いません。
-	自由にお書きください。
-\`\`\``);
+​コマンド：
+	>help    このメッセージを表示する
+	>init    初期設定を行う
+	>log     問題の一覧を表示する
+	>submit  問題を投稿する
+	>show    問題の詳細を表示する
+	>close   問題を閉じる
+	>issue   問題くんの問題を報告する
+\`\`\`
+​コマンドの詳細はこちらを参照してください。
+https://github.com/yuta0801/issues-kun/wiki/Command`);
 		});
 
 		// 初期設定
 		addCommand(message, /^>init$/, msg => {
-			db.dropCollection(message.channel.guild.id, (err, result) => {
-				message.channel.send((error)?'エラー：'+error:'削除が完了しました。');
-			});
-			db.createCollection(message.channel.guild.id, (err, col) => {
+			db.createCollection(message.channel.guild.id, (err, collection) => {
 				message.channel.send((error)?'エラー：'+error:'初期設定が完了しました。');
 			});
 		});
 
 		// 一覧表示
-		addCommand(message, /^>log( ([0-3]))?( (open|closed))?/, msg => {
+		addCommand(message, /^>log( ([0-3]))?( (open|closed))?( ([^#]{2,32}#\d{4})))$?/, msg => {
 			let list = [], collection = db.collection(message.channel.guild.id);
 			collection.find().toArray((err, docs) => {
 				for (let doc of docs) {
-					if (doc.status == 'open') {
-						list.push(`\`${doc.id}\`  ${level[doc.level]}  ${doc.title}  by ${doc.user}`);
-					}
+					if (msg[2] && doc.level != msg[2]) return;
+					if ((msg[4] && doc.status != msg[4]) || (!msg[4] && doc.status != 'open')) return;
+					if (msg[6] && doc.level != msg[6]) return;
+					list.push(`\`${doc.id}\`  ${level[doc.level]}  ${doc.title}  by ${doc.user}`);
 				}
-				message.channel.send((list.length>0)?list.join('\n'):'まだ投稿はありません！');
+				message.channel.send((list.length>0)?list.join('\n'):'見つかりませんでした！');
 			});
 		});
 
@@ -85,13 +77,11 @@ mongo.connect('mongodb://localhost:27017/issues', (error, db) => {
 			collection.findOne({id: msg[1]}, (err, doc) => {
 				console.log(doc);
 				message.channel.send(
-`
-\`${doc.id}\`  ${level[doc.level]}  ${doc.title}
+`\`${doc.id}\`  ${level[doc.level]}  ${doc.title}
 
 ${doc.content}
 
-by ${doc.user}  ${doc.status}  ${doc.date.toFormat('YYYY/MM/DD HH24:MI:SS')}
-`);
+by ${doc.user}  ${doc.status}  ${doc.date.toFormat('YYYY/MM/DD HH24:MI:SS')}`);
 			});
 		});
 
