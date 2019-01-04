5364a7732d60528a21fafa6f32efcc0e66e8ae3e
Yusuke Hayashi <yusuke8h@gmail.com>
2019-01-05 04:21:14 +0900

    [修正]Deploy時にnpm installするように修正

--- hooks/install.sh
+++ hooks/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd /home/ec2-user/repos/issues-discord-bot
+/home/ec2-user/.nvm/versions/node/v11.5.0/bin/npm install
 
-ZONE=$(curl 169.254.169.254/latest/meta-data/placement/availability-zone)
-REGION=$(echo ${ZONE/%?/})
+REGION=$(curl -s 169.254.169.254/latest/meta-data/local-hostname | cut -d '.' -f2)
 PARAMETER_NAME=ISSUES_DISCORD_BOT_SECRET
-echo "DISCORD_BOT_TOKEN=$(aws --region ${REGION} ssm get-parameters --name ${PARAMETER_NAME} --query "Parameters[0].Value" --output text)" > environment
+echo "DISCORD_BOT_TOKEN=$(aws --region ${REGION} ssm get-parameter --name ${PARAMETER_NAME} --query "Parameter.Value" --output text)" > environment
 
 sudo cp ./hooks/issues-discord-bot.service /etc/systemd/system/issues-discord-bot.service
 sudo /usr/bin/systemctl enable issues-discord-bot
