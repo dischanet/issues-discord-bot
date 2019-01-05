3db18cfe53664d5113871ecb87e572f30f902429
Yusuke Hayashi <yusuke8h@gmail.com>
2019-01-06 01:53:41 +0900

    Sqlite promisify (#2)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const Discord = require("discord.js"),
-  client = new Discord.Client();
+const util = require("util");
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
 require("date-utils");
 const sqlite3 = require("sqlite3").verbose();
@@ -15,6 +16,30 @@ db.run(
   date STRING,
   update_at STRING)`
 );
+
+const dbGet = util.promisify(db.get);
+const dbAll = util.promisify(db.all);
+const dbRun = util.promisify(db.run);
+
+const addCommand = async (message, cmd, callback) => {
+  const args = message.content.match(cmd);
+  if (!args) return;
+  try {
+    await callback(args);
+  } catch (error) {
+    message.reply("コマンド実行中にエラーが発生しました");
+  }
+};
+
+const findArr = (arr, cmd) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] && arr[i].match(cmd)) return arr[i];
+  }
+};
+
+const isOwner = message => {
+  return message.author.id === message.channel.guild.ownerID;
+};
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -44,119 +69,100 @@ https://github.com/yuta0801/issues-kun/wiki/Command`
   addCommand(
     message,
     /^\/issues\slog(\s(open|closed))?(\s([^#]{2,32}#\d{4}))?$/,
-    msg => {
+    async msg => {
       const list = [];
-      db.all(
+      const rows = await dbAll(
         "SELECT user, status FROM issues WHERE guild_id=?",
-        [message.channel.guild.id],
-        (err, rows) => {
-          if (err) return; // TODO
-          const args = [msg[2], msg[4]],
-            user = findArr(args, /^[^#]{2,32}#\d{4}$/),
-            stats = findArr(args, /^(open|closed)$/);
-          for (const doc of rows) {
-            if (user && doc[0] !== user) continue;
-            if ((stats && doc[1] !== stats) || (!stats && doc[1] !== "open"))
-              continue;
-            list.push(`\`${doc.id}\`  ${doc.title}  by ${doc.user}`);
-          }
-          message.channel.send(
-            list.length > 0 ? list.join("\n") : "見つかりませんでした！"
-          );
-        }
+        [message.channel.guild.id]
+      );
+      const args = [msg[2], msg[4]],
+        user = findArr(args, /^[^#]{2,32}#\d{4}$/),
+        stats = findArr(args, /^(open|closed)$/);
+      for (const doc of rows) {
+        if (user && doc[0] !== user) continue;
+        if ((stats && doc[1] !== stats) || (!stats && doc[1] !== "open"))
+          continue;
+        list.push(`\`${doc.id}\`  ${doc.title}  by ${doc.user}`);
+      }
+      message.channel.send(
+        list.length > 0 ? list.join("\n") : "見つかりませんでした！"
       );
     }
   );
 
   // 投稿
-  addCommand(message, /^\/issues\ssubmit\s(.{2,20})[\s\n]([\s\S]+)$/, msg => {
-    db.run(
-      "INSERT INTO issues(user,title,content,status,date) VALUES(?,?,?,?,?)",
-      [message.author.tag, msg[1], msg[2], "open", new Date()],
-      error => {
-        message.channel.send(error ? `エラー：${error}` : "投稿しました。");
-      }
-    );
-  });
+  addCommand(
+    message,
+    /^\/issues\ssubmit\s(.{2,20})[\s\n]([\s\S]+)$/,
+    async msg => {
+      await dbRun(
+        "INSERT INTO issues(user,title,content,status,date) VALUES(?,?,?,?,?)",
+        [message.author.tag, msg[1], msg[2], "open", new Date()]
+      );
+      message.channel.send("投稿しました。");
+    }
+  );
 
   // 修正
   addCommand(
     message,
     /^\/issues\srevise\s(\d+)\s(.{2,20})[\s\n]([\s\S]+)$/,
-    msg => {
-      db.get("SELECT user FROM issues WHERE id=?", [msg[1]], (err, row) => {
-        if (err) return; // TODO
-        if (!isOwner(message) && message.author.tag !== row[0]) {
-          message.channel.send(
-            "サーバーのオーナー以外は他人の投稿した問題を閉じることはできません！"
-          );
-          return;
-        }
-        db.get(
-          "UPDATE issues SET user=?, title=?, content=?, status=?, update_at=?",
-          [message.author.tag, msg[2], msg[3], "open", new Date()],
-          error => {
-            message.channel.send(
-              error ? `エラー：${error}` : `\`${msg[1]}\`を変更しました。`
-            );
-          }
+    async msg => {
+      const row = await dbGet("SELECT user FROM issues WHERE id=?", [msg[1]]);
+
+      if (!isOwner(message) && message.author.tag !== row[0]) {
+        message.channel.send(
+          "サーバーのオーナー以外は他人の投稿した問題を閉じることはできません！"
         );
-      });
+        return;
+      }
+      await dbRun(
+        "UPDATE issues SET user=?, title=?, content=?, status=?, update_at=?",
+        [message.author.tag, msg[2], msg[3], "open", new Date()]
+      );
+      message.channel.send(`\`${msg[1]}\`を変更しました。`);
     }
   );
 
   // 表示
-  addCommand(message, /^\/issues\sshow\s(\d+)$/, msg => {
-    db.get(
+  addCommand(message, /^\/issues\sshow\s(\d+)$/, async msg => {
+    const row = await dbGet(
       "SELECT id, title, content, user, status, date FROM issues WHERE id=?",
-      [msg[1]],
-      (err, row) => {
-        if (err) return; // TODO
-        message.channel.send(
-          `\`${row[0]}\`  ${row[1]}
+      [msg[1]]
+    );
+    message.channel.send(
+      `\`${row[0]}\`  ${row[1]}
 
 ${row[2]}
 
 by ${row[3]}  ${row[4]}  ${row[5]}`
-        );
-      }
     );
   });
 
   // 閉じる
-  addCommand(message, /^\/issues\sclose\s(\d+)$/, msg => {
-    db.get("SELECT user FROM issues WHERE id=?", [msg[1]], (err, row) => {
-      if (err) return; // TODO
-      if (!isOwner(message) && message.author.tag !== row[0]) {
-        message.channel.send(
-          "サーバーのオーナー以外は他人の投稿した問題を閉じることはできません！"
-        );
-        return;
-      }
-      db.get("UPDATE issues SET status=?", ["closed"], error => {
-        message.channel.send(
-          error ? `エラー：${error}` : `\`${msg[1]}\`を閉じました。`
-        );
-      });
-    });
+  addCommand(message, /^\/issues\sclose\s(\d+)$/, async msg => {
+    const row = dbGet("SELECT user FROM issues WHERE id=?", [msg[1]]);
+    if (!isOwner(message) && message.author.tag !== row[0]) {
+      message.channel.send(
+        "サーバーのオーナー以外は他人の投稿した問題を閉じることはできません！"
+      );
+      return;
+    }
+    await dbRun("UPDATE issues SET status=?", ["closed"]);
+    message.channel.send(`\`${msg[1]}\`を閉じました。`);
   });
 
   // 開く
-  addCommand(message, /^\/issues\sopen\s([a-zA-Z0-9]{8})$/, msg => {
-    db.get("SELECT user FROM issues WHERE id=?", [msg[1]], (err, row) => {
-      if (err) return; // TODO
-      if (!isOwner(message) && message.author.tag !== row[0]) {
-        message.channel.send(
-          "サーバーのオーナー以外は他人の投稿した問題を閉じることはできません！"
-        );
-        return;
-      }
-      db.get("UPDATE issues SET status=?", ["open"], error => {
-        message.channel.send(
-          error ? `エラー：${error}` : `\`${msg[1]}\`を開きました。`
-        );
-      });
-    });
+  addCommand(message, /^\/issues\sopen\s(\d+)$/, async msg => {
+    const row = await dbGet("SELECT user FROM issues WHERE id=?", [msg[1]]);
+    if (!isOwner(message) && message.author.tag !== row[0]) {
+      message.channel.send(
+        "サーバーのオーナー以外は他人の投稿した問題を閉じることはできません！"
+      );
+      return;
+    }
+    await dbRun("UPDATE issues SET status=?", ["open"]);
+    message.channel.send(`\`${msg[1]}\`を開きました。`);
   });
 });
 
@@ -170,17 +176,3 @@ client.login(process.env.DISCORD_BOT_TOKEN);
 //   }
 //   return arr.includes(r) ? makeId(arr) : r;
 // }
-
-function addCommand(message, cmd, callback) {
-  if (message.content.match(cmd)) return callback(message.content.match(cmd));
-}
-
-function findArr(arr, cmd) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] && arr[i].match(cmd)) return arr[i];
-  }
-}
-
-function isOwner(message) {
-  return message.author.id === message.channel.guild.ownerID;
-}
