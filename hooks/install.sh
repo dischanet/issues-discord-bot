2708da4abbd41ccbe0e26adc8ed97404676d57e8
Yusuke Hayashi <yusuke8h@gmail.com>
2019-01-05 15:27:44 +0900

    delpoy at codedeploy

--- hooks/install.sh
+++ hooks/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd /home/ec2-user/repos/issues-discord-bot
-/home/ec2-user/.nvm/versions/node/v11.5.0/bin/npm install
 
 REGION=$(curl -s 169.254.169.254/latest/meta-data/local-hostname | cut -d '.' -f2)
 PARAMETER_NAME=ISSUES_DISCORD_BOT_SECRET
